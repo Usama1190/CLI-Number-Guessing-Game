d33d8b4e0fec0e539d5210ada2bbed4d0833dc8b
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-03-22 12:47:09 -0700

    CLI Number Gussing Game in TypeScript, Node.js and inquirer

---
+++ index.js
@@ -0,0 +1,17 @@
+import inquirer from 'inquirer';
+const mechineRandom = Math.floor(Math.random() * 11);
+const mechineNumber = mechineRandom;
+const userGuessNumber = await inquirer.prompt([
+    {
+        name: 'guessNumber',
+        type: 'number',
+        message: 'Please guess number b/w 1 to 10'
+    },
+]);
+if (userGuessNumber.guessNumber === mechineNumber) {
+    console.log('You guessed right number');
+}
+else {
+    console.log('You guess wrong number');
+}
+console.log('Gessed number of mechine is: ' + mechineRandom);
